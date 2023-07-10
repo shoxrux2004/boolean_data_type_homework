47958f2d179ccabd07c938d29a1fb52b13a2e001
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-07-10 22:48:39 +0500

    toqlikni tekshirish

--- bool05.py
+++ bool05.py
@@ -1,5 +1,8 @@
 def main(a):
-    """
+    toq=a%2==1
+    return toq
+print(main(5))
+"""
     check the following statement "The variable "a" is an odd number"
     Args:
         a: int
@@ -7,4 +10,3 @@ def main(a):
         bool
     """
     # Write your code here
-    return 
